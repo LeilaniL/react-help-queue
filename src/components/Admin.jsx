e126230dbb0b552f36ec8c36d01b9784b3b83fcd
Leilani Leach <leilanileach@yahoo.com>
2019-02-21 10:01:33 -0800

    add Error route

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import TicketDetail from './TicketDetail';
+import React from "react";
+import PropTypes from "prop-types";
+import TicketDetail from "./TicketDetail";
 
 function Admin() {
-    return (
-        <div>
-            <h2>Admin</h2>
-            <TicketDetail />
-            <TicketList
-                ticketList={props.ticketList}
-                currentRouterPath={props.currentRouterPath} />
-        </div>
-    );
+  return (
+    <div>
+      <h2>Admin</h2>
+      <TicketDetail />
+      <TicketList
+        ticketList={props.ticketList}
+        currentRouterPath={props.currentRouterPath}
+      />
+    </div>
+  );
 }
 
 Admin.propTypes = {
-    ticketList: PropTypes.array
-}
+  ticketList: PropTypes.array
+};
 export default Admin;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Header from './Header';
-import TicketList from './TicketList';
-import NewTicketControl from './NewTicketControl';
-import Admin from './Admin';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import TicketList from "./TicketList";
+import NewTicketControl from "./NewTicketControl";
+import Error404 from "./Error404";
+import Admin from "./Admin";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,38 +12,58 @@ class App extends React.Component {
     this.state = {
       masterTicketList: []
     };
-    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(
+      this
+    );
   }
   updateTicketElapsedWaitTime() {
     console.log("check");
     let newMasterTicketList = this.state.masterTicketList.slice();
-    newMasterTicketList.forEach((ticket) =>
-      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true));
-    this.setState({ masterTicketList: newMasterTicketList })
+    newMasterTicketList.forEach(
+      ticket => (ticket.formattedWaitTime = ticket.timeOpen.fromNow(true))
+    );
+    this.setState({ masterTicketList: newMasterTicketList });
   }
   componentDidMount() {
-    this.waitTimeUpdateTimer = setInterval(() =>
-      this.updateTicketElapsedWaitTime(), 60000)
+    this.waitTimeUpdateTimer = setInterval(
+      () => this.updateTicketElapsedWaitTime(),
+      60000
+    );
   }
   componentWillUnmount() {
     clearInterval(this.waitTimeUpdateTimer);
   }
   handleAddingNewTicketToList(newTicket) {
     var newMasterTicketList = this.state.masterTicketList.slice();
-    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
+    newTicket.formattedWaitTime = newTicket.timeOpen.fromNow(true);
     newMasterTicketList.push(newTicket);
     this.setState({ masterTicketList: newMasterTicketList });
   }
   render() {
-
     return (
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' render={() => <TicketList ticketList={this.state.masterTicketList} />} />
-          <Route path='/newticket' render={() => <NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
-          <Route path='/admin' render={() => <Admin ticketList={this.state.masterTicketList} />} />
-          {/* <Route component={Error404} /> */}
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <TicketList ticketList={this.state.masterTicketList} />
+            )}
+          />
+          <Route
+            path="/newticket"
+            render={() => (
+              <NewTicketControl
+                onNewTicketCreation={this.handleAddingNewTicketToList}
+              />
+            )}
+          />
+          <Route component={Error404} />
+          <Route
+            path="/admin"
+            render={() => <Admin ticketList={this.state.masterTicketList} />}
+          />
         </Switch>
       </div>
     );
